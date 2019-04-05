b405b14622e512264ed2e25bc40c4934671c65eb
Eimer Wilfer Castro Hincapie <eimer.castro@globant.com>
2019-04-05 16:45:29 -0500

    Real application using controller, services, repositories. Working with CRUD.

---
+++ src/main/java/com/udemy/backendninja/converter/ContactConverter.java
@@ -0,0 +1,28 @@
+package com.udemy.backendninja.converter;
+
+import org.springframework.stereotype.Component;
+
+import com.udemy.backendninja.entity.Contact;
+import com.udemy.backendninja.model.ContactModel;
+
+@Component("contactConverter")
+public class ContactConverter {
+
+	public Contact convertContactModelToContact(ContactModel contactModel) {
+		Contact contact = new Contact();
+		contact.setFirstname(contactModel.getFirstname());
+		contact.setLastname(contactModel.getLastname());
+		contact.setTelephone(contactModel.getTelephone());
+		contact.setCity(contactModel.getCity());
+		return contact;
+	}
+	
+	public ContactModel convertContactToContactModel(Contact contact) {
+		ContactModel contactModel = new ContactModel();
+		contactModel.setFirstname(contact.getFirstname());
+		contactModel.setLastname(contact.getLastname());
+		contactModel.setTelephone(contact.getTelephone());
+		contactModel.setCity(contact.getCity());
+		return contactModel;
+	}
+}
---
+++ src/main/java/com/udemy/backendninja/service/impl/ContactServiceImpl.java
@@ -0,0 +1,53 @@
+package com.udemy.backendninja.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.udemy.backendninja.converter.ContactConverter;
+import com.udemy.backendninja.entity.Contact;
+import com.udemy.backendninja.model.ContactModel;
+import com.udemy.backendninja.repository.ContactRepository;
+import com.udemy.backendninja.service.ContactService;
+
+@Service("contactServiceImpl")
+public class ContactServiceImpl implements ContactService {
+
+	@Autowired
+	@Qualifier("contactRepository")
+	private ContactRepository contactRepository;
+	
+	@Autowired
+	@Qualifier("contactConverter")
+	private ContactConverter contactConverter;
+	
+	@Override
+	public ContactModel addContact(ContactModel contactModel) {
+		Contact contact = contactRepository.save(contactConverter.convertContactModelToContact(contactModel));
+		return contactConverter.convertContactToContactModel(contact);
+	}
+
+	@Override
+	public List<ContactModel> listAllContacts() {
+		List<Contact> contacts = contactRepository.findAll();
+		List<ContactModel> contactModels = contacts.stream().map(contact -> contactConverter.convertContactToContactModel(contact)).collect(Collectors.toList());
+		return contactModels;
+	}
+
+	@Override
+	public Contact findContactById(int id) {
+		return contactRepository.findById(id);
+	}
+
+	@Override
+	public void removeContact(int id) {
+		Contact contact = findContactById(id); 
+		if(null != contact) {
+			contactRepository.delete(contact);
+		}		
+	}
+
+}
